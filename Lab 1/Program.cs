157c515fb83436b1b867556a6dee3de9a47d2f6e
shane <shanechastain1@ymail.com>
2023-07-10 14:32:43 -0400

    first commit for lab 1

---
+++ Lab 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Welcome! Please enter a width of your room.");
+double width = double.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the Length of your room.");
+double length = double.Parse(Console.ReadLine());
+Console.WriteLine("Please enter a height of your room.");
+double height =  double.Parse(Console.ReadLine());
+
+double perimeter = (width * 2) + (height * 2);
+double area = height * width;
+double volume = area * height;
+
+double a = (area * 2);
+double b = (width * height * 2);
+double c = (length * height * 2);
+double surfArea = a + b + c;
+
+Console.WriteLine("The area of the room is " + area + ". The perimeter of the room is " + perimeter + ". The surface area of the room is " + surfArea + ". The volume of the room is " + volume + ".");
+
+if (area <= 250)
+{
+    Console.WriteLine("The room is small.");
+}
+else if (area < 650 && area > 250)
+{
+    Console.WriteLine("The room is medium.");
+}
+else if (area >= 650)
+    Console.WriteLine("The room is large.");
